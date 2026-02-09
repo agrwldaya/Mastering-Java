5a2dc9a50ba026c45e6b1ce0d5691ab0caa03443
agrwldaya <agrawaldaya336@gmail.com>
2026-02-09 08:44:04 +0530

    type conversion and casting

---
+++ Typeconv_Casting/typecast.java
@@ -0,0 +1,27 @@
+class typecast{
+
+    public static void main(String m[]){
+
+        // int a = 300;
+        // byte b = (byte)a;
+
+        // int a = 1000000;
+        // int b = 1000000;
+        // int  res1 = a*b;
+        // long res2 = (long)(a*b);
+        // long res3 = (long)a*b;
+
+         // type pramotions
+        byte b1 = 10;
+        byte b2 = 30;
+
+        int b3 = b1*b2;
+
+
+        // System.out.println(res1);
+        // System.out.println(res2);
+        // System.out.println(res3);
+
+        System.out.println(b3);
+    }
+}
