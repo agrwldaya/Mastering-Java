353984a36fbeb2e992445d2d3a5e8ba494cc1dbb
agrwldaya <agrawaldaya336@gmail.com>
2026-02-09 08:25:24 +0530

    first commit

---
+++ Datatypes/hello.java
@@ -0,0 +1,28 @@
+// ------ Data Types-------
+
+// integer = int(4 byte), long (8 byte), short(2 byte), byte(1 byte) , (--use l for long in last)
+// Charcter =  char(2 byte)
+// float =  float(4 byte), double(8 byte) -- use f for float in last
+// boolean = true and false (boolean b = true)
+
+class hello{
+
+    public static void main(String a[]){
+        
+
+        int b1 = 0b1010;
+        System.out.println(b1);
+
+        char c = 'k';
+        double d = 2943.00; 
+        float f = 3432.89f; // use f at last to convert into float
+        long l = 24243l; // use (l) at last to convert into float
+
+
+        // int num1 = 5;
+        // int num2 = 6;
+        // int res = num1+num2;
+        // System.out.println(res);
+        // System.out.println("Hello world!");
+    }
+}
