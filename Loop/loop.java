f1274ce2f420b11f27957c31c94b12d7eb0ffdea
agrwldaya <agrawaldaya336@gmail.com>
2026-02-10 06:52:53 +0530

    learned loops

---
+++ Loop/loop.java
@@ -0,0 +1,16 @@
+class loop{
+
+    public static void main(String m[]){
+
+        // for loop 
+        // for(int i=0;i<5;i++){
+        //     System.out.print(i+" ");
+        // }
+
+        // while loop
+
+          // int i = 0;
+        // while(i++<5) System.out.print(i+" ");
+
+    }
+}
